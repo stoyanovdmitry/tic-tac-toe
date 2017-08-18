bbec6ab461357032b83f4a5ea145b0025472a1cf
stoyanovdmitry <stoyanov.dmt@gmail.com>
2017-08-18 15:36:34 +0300

    added GameUtil

---
+++ src/main/java/com/stoyanovdmitry/util/GameUtil.java
@@ -0,0 +1,38 @@
+package com.stoyanovdmitry.util;
+
+import com.stoyanovdmitry.game.Game;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+public class GameUtil {
+
+	@Resource
+	private Map<String, Game> games;
+
+	public String addNewGame() {
+
+		String gameID = RandomStringUtils.randomAlphanumeric(5);
+
+		games.put(gameID, new Game());
+
+		return gameID;
+	}
+
+	public int addPlayerToGame(String gameID) {
+
+		Game game = getGameById(gameID);
+		return game.addPlayer();
+	}
+
+	public int[] stepGameById(String gameID, int pos, int playerID) {
+
+		return getGameById(gameID).step(pos, playerID);
+	}
+
+	private Game getGameById(String gameID) {
+
+		return games.get(gameID);
+	}
+}
